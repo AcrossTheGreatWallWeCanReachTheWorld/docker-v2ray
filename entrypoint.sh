3620a81bfc4fed972dcbc6fd224244d431dc62e1
AcrossTheGreatWallWeCanReachTheWorld <61279050+AcrossTheGreatWallWeCanReachTheWorld@users.noreply.github.com>
2020-02-21 17:43:28 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,7 +19,7 @@ server {
     listen 80 default_server;
     charset utf-8;
 
-    location /$REVERSE_PROXY_URL {
+    location /$V2RAY_PATH {
     proxy_redirect off;
     proxy_pass http://127.0.0.1:8080;
     proxy_http_version 1.1;
